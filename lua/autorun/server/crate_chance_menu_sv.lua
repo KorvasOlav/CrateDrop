95fb39d63ce94b9c5d54b001aa9c937642bbc194
KorvasOlav <88300986+KorvasOlav@users.noreply.github.com>
2023-06-20 08:10:10 -0700

    Update crate_chance_menu_sv.lua
    Added ability to have crate chance on an npc by npc basis

--- lua/autorun/server/crate_chance_menu_sv.lua
+++ lua/autorun/server/crate_chance_menu_sv.lua
@@ -1,18 +1,6 @@
-include("hooks.lua")
+npcCrateChances = {}
 
-concommand.Add("npcmenu", function(ply)
-        local npcClasses = GetNPCClasses()
-        net.Start("OpenNPCMenu")
-        net.WriteTable(npcClasses)
-        net.Send(ply)
-    end)
-
-if ( SERVER) then
-    
-    util.AddNetworkString("OpenNPCMenu")
-    util.AddNetworkString("SetNPCCrateChance")
-    util.AddNetworkString("GetNPCCrateChance")
-
+if SERVER then
     local function LoadCrateChances()
         if not file.Exists("crate_chances.txt", "DATA") then
             return {}
@@ -27,54 +15,7 @@ if ( SERVER) then
         file.Write("crate_chances.txt", contents)
     end
 
-    local crateChances = LoadCrateChances()
-
-    net.Receive("GetNPCCrateChance", function(_, ply)
-        local selectedNPCClass = net.ReadString()
-        local crateChance = crateChances[selectedNPCClass] or 0
-
-        net.Start("SetNPCCrateChance")
-        net.WriteInt(crateChance, 32)
-        net.Send(ply)
-    end)
-
-    net.Receive("SetNPCCrateChance", function(_, ply)
-        local selectedNPCClass = net.ReadString()
-        local newValue = net.ReadInt(32)
-        npcCrateChances[selectedNPCClass] = newValue
-
-        crateChances[selectedNPCClass] = newValue
-        SaveCrateChances(crateChances)
-    end)
-
-    function GetNPCClasses()
-        local npcClasses = {}
-        for npcClass, npcInfo in pairs(list.Get("NPC")) do
-            table.insert(npcClasses, {
-                class = npcClass,
-                name = npcInfo.Name or npcClass,
-                category = npcInfo.Category or "Unknown"
-            })
-        end
-        return npcClasses
-    end
-
-
-        local function LoadCrateChances()
-        if not file.Exists("crate_chances.txt", "DATA") then
-            return {}
-        end
-
-        local contents = file.Read("crate_chances.txt", "DATA")
-        return util.JSONToTable(contents) or {}
-    end
-
-    local function SaveCrateChances(crateChances)
-        local contents = util.TableToJSON(crateChances)
-        file.Write("crate_chances.txt", contents)
-    end
-
-    local npcCrateChances = LoadCrateChances()
+    npcCrateChances = LoadCrateChances()
 
     local function InitializeNPCCrateChances()
         for npcClass, _ in pairs(list.Get("NPC")) do
@@ -89,12 +30,21 @@ if ( SERVER) then
     -- When an NPC is killed, roll a chance to determine if a crate will drop
     hook.Add("OnNPCKilled", "NPCDropCrate", function(npc, attacker, inflictor)
         local npcClass = npc:GetClass()
-        local crateChance = npcCrateChances[npcClass] or 0
-
+        local crateChance = 0
+    
+        local npcNWFloat = npc:GetNWFloat("CrateChance")
+        if npcNWFloat ~= 0 then
+            crateChance = npcNWFloat
+        else
+            crateChance = npcCrateChances[npcClass] or 0
+        end
+        print(crateChance)
         -- Roll a chance based on crateChance value
-        if math.random() <= crateChance then
+        if math.random(0, 100) <= crateChance then
+            local spawnPosition = npc:GetPos() + Vector(0, 0, 10)
+    
             local crate = ents.Create("npc_drop_crate")
-            crate:SetPos(npc:GetPos())
+            crate:SetPos(spawnPosition)
             crate:Spawn()
         end
     end)
